84c3f5ff108accab0a8907e8947bcabf7573bbe9
zoruss73 <russeljarcia07@gmail.com>
2026-03-05 12:56:40 +0800

    feat: add sonner toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AuthLayout from "./components/layouts/AuthLayout";
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Toaster position="top-right" richColors theme="dark" />
       <Routes>
         <Route element={<AuthLayout />}>
           <Route path="/" element={<Login />} />
